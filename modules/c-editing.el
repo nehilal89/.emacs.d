a7c99d30f7a099383c02b68b612db8a0d949caa5
Rob Blafford <rob@concord.io>
2015-12-07 14:51:37 -0500

    Add makefile rules

--- modules/c-editing.el
+++ modules/c-editing.el
@@ -6,18 +6,25 @@
 (require 'clang-format)
 
 ;; Personal settings when in c mode
-(defun my-c-mode-common-hook ()
-  (c-set-style "gnu")
+(defun rb-c-mode-common-hook ()
+  (setq c-default-style "gnu"
+	c-basic-offset 2)
   (c-set-offset 'innamespace 0)
-  (setq-default subword-mode 1)
-  (setq tab-width 4 indent-tabs-mode nil))
+  (setq-default subword-mode 1))
 
 ;; Hooks to enable functionality when in c/c++ mode only
-(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+(add-hook 'c-mode-common-hook 'rb-c-mode-common-hook)
 (add-hook 'c-mode-common-hook
 	  (lambda () (local-set-key (kbd "M-8") 'compile)))
 (add-hook 'c++-mode-hook
 	  (lambda () (local-set-key (kbd "C-x C-s") #'clang-format-region)))
+
+
+(defun rb-makefile-mode-defaults ()
+  (whitespace-toggle-options '(tabs))
+  (setq indent-tabs-mode t ))
+
+(add-hook 'makefile-mode-hook 'rb-makefile-mode-defaults)
 
 (provide 'c-editing)
 
